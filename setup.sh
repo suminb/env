62edeba2ba0d76ba55ed1efdb4b7dc49e6a27553
Sumin Byeon <suminb@nexon.co.kr>
2016-01-27 04:20:03 +0000

    Install zsh and git as necessary

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+command_exists() {
+    which "$1" &> /dev/null
+}
+
 BASE_PATH=$(dirname $0)
+
+if ! command_exists zsh; then
+    sudo apt-get install -y zsh
+fi
+if ! command_exists git; then
+    sudo apt-get install -y git
+fi
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
